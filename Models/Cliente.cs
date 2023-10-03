44c56912048106143d4ffb3572539b2e7884c949
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2023-10-03 10:42:06 -0300

    commit

---
+++ Models/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace aplicacaoLoja.Models
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        [Key]
+        [Display(Name = "ID")]
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public int id { get; set; }
+
+        [Display(Name = "Nome")]
+        [StringLength(60)]
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public string nome { get; set; }
+
+        [Display(Name = "Telefone")]
+        [StringLength(15)]
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public string telefone { get; set; }
+
+        [Display(Name = "CPF")]
+        [StringLength(14)]
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public string cpf { get; set; }
+    }
+}
