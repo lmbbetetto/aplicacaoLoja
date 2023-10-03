800450955da168c7faa005e0bd179fc297d034b8
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2023-10-02 22:12:13 -0300

    Criação classe "Clientes"

--- Models/Contexto.cs
+++ Models/Contexto.cs
@@ -5,5 +5,6 @@ namespace aplicacaoLoja.Models
     public class Contexto : DbContext
     {
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Cliente> Clientes { get; set; }
     }
 }
