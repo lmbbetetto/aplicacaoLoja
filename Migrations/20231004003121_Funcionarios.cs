308c5d0daa018e6cf2344918701a49aceca2a398
Leonardo Betetto <60801645+lmbbetetto@users.noreply.github.com>
2023-10-03 21:32:44 -0300

    commit

---
+++ Migrations/20231004003121_Funcionarios.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace aplicacaoLoja.Migrations
+{
+    /// <inheritdoc />
+    public partial class Funcionarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "Funcionarios",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double>(
+                name: "salario",
+                table: "Funcionarios",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "Funcionarios");
+
+            migrationBuilder.DropColumn(
+                name: "salario",
+                table: "Funcionarios");
+        }
+    }
+}
